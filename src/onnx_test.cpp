abe7704eb39d654b479b2137efcb62555ca75d28
neverorfrog <97flavio.maiorana@gmail.com>
2024-05-22 17:03:30 +0200

    intermedio

--- src/onnx_test.cpp
+++ src/onnx_test.cpp
@@ -7,37 +7,38 @@ using std::cout, std::endl;
 
 int main() {
     // Allocate ONNXRuntime session
+    Ort::AllocatorWithDefaultOptions allocator = Ort::AllocatorWithDefaultOptions();
     auto memory_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
     Ort::Env env;
-    Ort::Session session{env, ORT_TSTR("cnn.onnx"), Ort::SessionOptions{nullptr}};
+    Ort::Session session{env, ORT_TSTR("models/cnn.onnx"), Ort::SessionOptions{nullptr}};
 
     size_t numInputNodes = session.GetInputCount();
     size_t numOutputNodes = session.GetOutputCount();
     std::cout << "Number of input nodes: " << numInputNodes << "\n";
     std::cout << "Number of output nodes: " << numOutputNodes << "\n";
 
-    Ort::AllocatorWithDefaultOptions allocator;
     const char* inputName = session.GetInputNameAllocated(0, allocator).get();
-    std::cout << "Input name: " << inputName << "\n";
+    printf("Input name: %s\n", inputName);
+    // std::cout << "Input name: " << std::string{inputName} << "\n";
     const char* outputName = session.GetOutputNameAllocated(0, allocator).get();
     std::cout << "Output name: " << outputName << "\n";
 
-    // Allocate model inputs: fill in shape and size
-    std::array<float, 784> input{};
-    std::array<int64_t, 4> input_shape{1,1,28,28};
-    Ort::Value input_tensor = Ort::Value::CreateTensor<float>(memory_info, input.data(), input.size(), input_shape.data(), input_shape.size());
-    const char* input_names[] = {inputName};
+    // // Allocate model inputs: fill in shape and size
+    // std::array<float, 784> input{};
+    // std::array<int64_t, 4> input_shape{1,1,28,28};
+    // Ort::Value input_tensor = Ort::Value::CreateTensor<float>(memory_info, input.data(), input.size(), input_shape.data(), input_shape.size());
+    // const char* input_names[] = {inputName};
 
     // Allocate model outputs: fill in shape and size
-    std::array<float, 10> output{};
-    std::array<int64_t, 2> output_shape{10,1};
-    Ort::Value output_tensor = Ort::Value::CreateTensor<float>(memory_info, output.data(), output.size(), output_shape.data(), output_shape.size());
-    const char* output_names[] = {outputName};
+    // std::array<float, 10> output{};
+    // std::array<int64_t, 2> output_shape{10,1};
+    // Ort::Value output_tensor = Ort::Value::CreateTensor<float>(memory_info, output.data(), output.size(), output_shape.data(), output_shape.size());
+    // const char* output_names[] = {outputName};
 
-    Ort::TypeInfo inputTypeInfo = session.GetInputTypeInfo(0);
-    auto inputTensorInfo = inputTypeInfo.GetTensorTypeAndShapeInfo();
-    std::vector<int64_t> inputDims = inputTensorInfo.GetShape();
-    std::cout << "Input Dimensions: " << inputDims << std::endl;
+    // Ort::TypeInfo inputTypeInfo = session.GetInputTypeInfo(0);
+    // auto inputTensorInfo = inputTypeInfo.GetTensorTypeAndShapeInfo();
+    // std::vector<int64_t> inputDims = inputTensorInfo.GetShape();
+    // std::cout << "Input Dimensions: " << inputDims << std::endl;
 
     // Run the model
     // session.Run(Ort::RunOptions{nullptr}, input_names, &input_tensor, 1, output_names, &output_tensor, 1);
