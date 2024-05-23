166831710031a79a9b097021cc181f5b12a551aa
neverorfrog <97flavio.maiorana@gmail.com>
2024-05-23 15:39:02 +0200

    removed low-level stuff

--- src/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "et_runner.h"
-#include <iostream>
-
-int main() {
-    std::string path = "models/cnn.pte";
-    const char* model_path = path.c_str();
-    Runner runner = Runner(model_path);
-    return 0;
-} 
